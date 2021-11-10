812eb7bb398abf797d13e04038e8266086689d6c
gail enstice <skyedoll@aol.com>
2021-11-09 21:58:33 -0500

    added some comments

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,7 @@
 /*
   This example requires Tailwind CSS v2.0+ 
   
-  This example requires some changes to your config:
+  //This example requires some changes to your config:
   
   ```
   // tailwind.config.js
