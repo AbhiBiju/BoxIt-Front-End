296cf395b9737f744a1353bf7a069ecb5a33356c
Sirmaaalik <malikrharris@gmail.com>
2021-11-22 01:56:23 -0500

    Merge branch 'ralph/home/signup/branch' of github.com:AbhiBiju/BoxIt-Front-End into malik

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,6 +13,29 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <footer aria-labelledby="footer-heading" className="flex justify-between bg-purple-500 bg-gradient-to-t from-purple-700 border-t border-gray-200 py-10 px-8">
+        <p className="text-white">
+          © BoxIt 2021:        
+          <span className="ml-2 py-1 px-2 bg-green-100 rounded-lg border text-gray-800 border-gray-900 hover:bg-green-300">
+            Abhinav Biju
+          </span>
+          <span className="ml-2 py-1 px-2 bg-green-100 rounded-lg border text-gray-800 border-gray-900 hover:bg-green-300">
+            Ralph Pierre
+          </span>
+          <span className="ml-2 py-1 px-2 bg-green-100 rounded-lg border text-gray-800 border-gray-900 hover:bg-green-300">
+            Catherine Clifford
+          </span>
+          <span className="ml-2 py-1 px-2 bg-green-100 rounded-lg border text-gray-800 border-gray-900 hover:bg-green-300">
+            Malik Harris
+          </span>
+          <span className="ml-2 py-1 px-2 bg-green-100 rounded-lg border text-gray-800 border-gray-900 hover:bg-green-300">
+            Tysean Hill
+          </span>
+        </p>
+        <a href="contact" className="text-white">
+          Contact Us
+        </a>
+      </footer>
         </body>
       </Html>
     );
